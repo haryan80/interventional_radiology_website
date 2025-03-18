e43dc3a76b0ad827a0cfab4b04d0bd71371e85e7
haryan80 <46519659+haryan80@users.noreply.github.com>
2025-03-18 13:15:03 +0300

    Update settings.py

--- khcc_conference/settings.py
+++ khcc_conference/settings.py
@@ -136,7 +136,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'khcc.ioc2025@gmail.com'
-EMAIL_HOST_PASSWORD = 'KHCC.123456'  # Consider using environment variables for this in production
+EMAIL_HOST_PASSWORD = 'mtxj kfio irbq mtqr'  # Consider using environment variables for this in production
 DEFAULT_FROM_EMAIL = 'khcc.ioc2025@gmail.com'
 SERVER_EMAIL = 'khcc.ioc2025@gmail.com'
 
