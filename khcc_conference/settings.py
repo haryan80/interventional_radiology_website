0402bd24199213562c0137d3fa33177cffe5c8f7
haryan80 <46519659+haryan80@users.noreply.github.com>
2025-03-18 12:23:06 +0300

    Update settings.py

--- khcc_conference/settings.py
+++ khcc_conference/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@_%-(@+di+r6o32@psdlu78q4y@x&&zvf6%ttb0-(p&4x%_(kp"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.khcc-ioc.org', 'khcc-ioc.org', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.khcc-ioc.org', 'khcc-ioc.org', 'localhost', '127.0.0.1','www.interventional-radiology.azurewebsites.net']
 
 
 # Application definition
